35a1f5a570c09316f564c2a23cb3205b2008580f
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-08 17:27:07 -0600

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
-var myName = "Paul";
-var lastName = "Anigbo";
-var luckyNumber = 7;
-var date = new Date();
+import App from "./components/App";
 
-const customStyles = {
-  border: "1px solid red",
-  color: "red",
-  textAlign: "center",
-  paddingTop: "10px"
-};
-
-let greetings;
-
-const currentDate = new Date();
-
-if (currentDate.getHours() < 12) {
-  greetings = "Good Morning! ";
-  customStyles.color = "red";
-  customStyles.border = "1px solid red";
-} else if (currentDate.getHours() > 12 && currentDate.getHours() < 18) {
-  greetings = "Good Afternoon! ";
-  customStyles.color = "green";
-  customStyles.border = "1px solid green";
-} else {
-  greetings = "Good Evening! ";
-  customStyles.color = "blue";
-  customStyles.border = "1px solid blue";
-}
-
-ReactDOM.render(
-  <div>
-    <Heading>{greetings}</Heading>
-    <List></List>
-    <h1 style={customStyles} id="gretingText">
-      {greetings}
-    </h1>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
